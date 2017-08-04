cc6394d20d035a70ae85e767ac9f7d18693e36c0
Grant Willison <emailgdw@gmail.com>
2017-08-04 09:58:57 -0700

    sandeep passed broken code, merging in fixes

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
 import { bindActionCreators } from 'redux';
-import { retrieveOrgs } from './actions/orgActions';
+// import { retrieveOrgs } from '../actions/orgActions';
 import { connect } from 'react-redux';
 import Issues from './Issues/Issues';
 import PrTable from './PrTable/PrTable';
